75562e66258ad178008b4d2212ed9bb66fd758d3
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2025-01-23 21:54:52 +0800

    Completed Lesson 12
    - Lesson 12: Render passes

--- HelloTriangle/main.cpp
+++ HelloTriangle/main.cpp
@@ -90,6 +90,8 @@ private:
 
     std::vector<VkImageView> m_swapChainImageViews;
 
+    VkRenderPass m_renderPass;
+  
     VkPipelineLayout m_pipelineLayout;
 private:
     void initWindow() 
@@ -115,6 +117,7 @@ private:
         createLogicalDevice();
         createSwapChain();
         createImageViews();
+        createRenderPass();
         createGraphicsPipeline();
     }
 
@@ -128,7 +131,7 @@ private:
     void cleanup()
     {
         vkDestroyPipelineLayout(m_device, m_pipelineLayout, nullptr);
-
+        vkDestroyRenderPass(m_device, m_renderPass, nullptr);
         for (const VkImageView& imageView : m_swapChainImageViews) {
             vkDestroyImageView(m_device, imageView, nullptr);
         }
@@ -706,6 +709,39 @@ private:
 
         return buffer;
 
+    }
+
+    void createRenderPass() 
+    {
+        VkAttachmentDescription colorAttachment{};
+        colorAttachment.format = m_swapChainImageFormat;
+        colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+        colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+        colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+        colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+        colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+        colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+        colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+        VkAttachmentReference colorAttachmentRef{};
+        colorAttachmentRef.attachment = 0;
+        colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+        VkSubpassDescription subpass{};
+        subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+        subpass.colorAttachmentCount = 1;
+        subpass.pColorAttachments = &colorAttachmentRef;
+
+        VkRenderPassCreateInfo renderPassInfo{};
+        renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+        renderPassInfo.attachmentCount = 1;
+        renderPassInfo.pAttachments = &colorAttachment;
+        renderPassInfo.subpassCount = 1;
+        renderPassInfo.pSubpasses = &subpass;
+
+        VkResult renderPassCreateResult = vkCreateRenderPass(m_device, &renderPassInfo, nullptr, &m_renderPass);
+        if (renderPassCreateResult != VK_SUCCESS)
+            throw std::runtime_error("Failed to create render pass!");
     }
 };
 
